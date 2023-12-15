14a35cf110e7e2964ade21947622aab298dbee50
Oatchariyanut <Oatchariyanut.38802@gmail.com>
2023-12-15 10:16:12 +0700

    changed name to thai language

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,10 +9,10 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อ-สกุลของคุณ *"
+        hint="ใส่ชื่อและสกุล"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'กรุณาใส่ชื่อและสกุล']"
       />
 
       <q-input
